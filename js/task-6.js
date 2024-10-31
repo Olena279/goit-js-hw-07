cfe603309a966158e8891d782c63e4b739c11ea7
Olenka <Riabova_o@ukr.net>
2024-10-31 17:44:51 +0200

    update js-task-6

--- js/task-6.js
+++ js/task-6.js
@@ -6,15 +6,17 @@ function getRandomHexColor() {
 
 function createBoxes(amount) {
   const boxesContainer = document.getElementById(`.boxes`);
-  boxesContainer.innerHTML = ``;
+  const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < amount; i++) {
     const div = document.createElement(`div`);
     div.style.width = `${30 + i * 10}px`;
     div.style.height = `${30 + i * 10}px`;
     div.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(div);
+    fragment.appendChild(div);
   }
+
+  boxesContainer.appendChild(fragment);
 }
 
 function destroyBoxes() {
@@ -22,17 +24,17 @@ function destroyBoxes() {
   boxesContainer.innerHTML = ``;
 }
 
-const controls = document.getElementById(`controls`);
-controls.addEventListener(`click`, event => {
-  const input = document.querySelector(`input`);
+const input = document.querySelector(`input`);
+const createBtn = document.querySelector(`button[data-create]`);
+const destroyBtn = document.querySelector(`button[data-destroy]`);
+
+createBtn.addEventListener(`click`, () => {
   const amount = Number(input.value);
 
-  if (event.target.dataset.create) {
-    if (amount >= 1 && amount <= 100) {
-      createBoxes(amount);
-      input.value = ``;
-    }
-  } else if (event.target.dataset.destroy) {
-    destroyBoxes();
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ``;
   }
 });
+
+destroyBtn.addEventListener(`click`, destroyBoxes);
